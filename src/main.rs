b7ba449c582a4da1d3765508be2f0a1ef9089ef0
Mikazuki <mikazuki_fuyuno@outlook.com>
2019-07-27 03:56:48 +0900

    refactor: extract as fn

--- src/main.rs
+++ src/main.rs
@@ -38,38 +38,42 @@ fn read_from_file(path: &str, pattern: regex::Regex) -> Result<(), String> {
     let file = File::open(path).map_err(|e| e.to_string())?; // std::io::Error to String
     let mut reader = BufReader::new(file);
     let mut buffer = String::new();
-    let mut color = ColorSpec::new();
 
     while reader.read_line(&mut buffer).map_err(|e| e.to_string())? > 0 {
-        if pattern.is_match(&buffer) {
-            let mut stdout = StandardStream::stdout(ColorChoice::Always);
-            let mut pos = 0;
-
-            for mat in pattern.find_iter(&buffer) {
-                if pos <= mat.start() {
-                    // write buffer
-                    write!(&mut stdout, "{}", &buffer[pos..mat.start()]).unwrap();
-                }
-
-                // highlight keywords
-                color.set_fg(Some(Color::Green));
-                color.set_bold(true);
-                stdout.set_color(&mut color).map_err(|e| e.to_string())?;
-                write!(&mut stdout, "{}", &buffer[mat.start()..mat.end()]).unwrap();
-
-                // reset color
-                color.clear();
-                stdout.set_color(&mut color).map_err(|e| e.to_string())?;
-                pos = mat.end();
-            }
-
-            if pos < buffer.len() {
-                write!(&mut stdout, "{}", &buffer[pos..]).unwrap();
-            }
-        } else {
-            print!("{}", buffer);
-        }
+        print_line(&buffer, &pattern)?;
         buffer.clear();
+    }
+
+    return Ok(());
+}
+
+fn print_line(buffer: &str, pattern: &regex::Regex) -> Result<(), String> {
+    if !pattern.is_match(&buffer) {
+        print!("{}", buffer);
+        return Ok(());
+    }
+
+    let mut stdout = StandardStream::stdout(ColorChoice::Always);
+    let mut color = ColorSpec::new();
+    let mut pos = 0;
+
+    for mat in pattern.find_iter(&buffer) {
+        if pos < mat.start() {
+            write!(&mut stdout, "{}", &buffer[pos..mat.start()]).unwrap();
+        }
+
+        color.set_fg(Some(Color::Green));
+        color.set_bold(true);
+        stdout.set_color(&mut color).map_err(|e| e.to_string())?;
+        write!(&mut stdout, "{}", &buffer[mat.start()..mat.end()]).unwrap();
+
+        color.clear();
+        stdout.set_color(&mut color).map_err(|e| e.to_string())?;
+        pos = mat.end();
+    }
+
+    if pos < buffer.len() {
+        write!(&mut stdout, "{}", &buffer[pos..]).unwrap();
     }
 
     return Ok(());
