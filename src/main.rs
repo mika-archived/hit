8f8fdd9912ccc32d3a8e43db37f3e0497f16ed10
Mikazuki <mikazuki_fuyuno@outlook.com>
2019-07-27 16:05:29 +0900

    feat: read from pipe

--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,11 @@ extern crate regex;
 extern crate termcolor;
 
 use std::fs::File;
-use std::io::{BufRead, BufReader, Write};
+use std::io::{stdin, BufRead, BufReader, Read, Write};
 use std::path::Path;
+use std::str::from_utf8;
 
+use atty::Stream;
 use regex::Regex;
 use termcolor::{Color, ColorChoice, ColorSpec, StandardStream, WriteColor};
 
@@ -21,6 +23,8 @@ fn main() -> Result<(), String> {
 
     if let Some(path) = matches.value_of("PATH") {
         return read_from_file(path, pattern);
+    } else if !atty::is(Stream::Stdin) {
+        return read_from_pipe(pattern);
     }
 
     return Ok(());
@@ -44,6 +48,24 @@ fn read_from_file(path: &str, pattern: Regex) -> Result<(), String> {
         buffer.clear();
     }
 
+    return Ok(());
+}
+
+fn read_from_pipe(pattern: Regex) -> Result<(), String> {
+    let stdin = stdin();
+    let mut lockd = stdin.lock();
+    let mut buffer = [0u8; 1024];
+
+    loop {
+        match lockd.read(&mut buffer) {
+            Ok(0) => break,
+            Ok(n) => {
+                let string = from_utf8(&buffer[0..n]).map_err(|e| e.to_string())?;
+                print_line(&string, &pattern)?;
+            }
+            Err(e) => return Err(e.to_string()),
+        }
+    }
     return Ok(());
 }
 
