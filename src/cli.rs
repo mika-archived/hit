2a283ced0b416d336b815d5fa20dce2d611d89cf
Mikazuki <mikazuki_fuyuno@outlook.com>
2019-08-01 08:39:53 +0900

    feat: accept multiple patterns

--- src/cli.rs
+++ src/cli.rs
@@ -1,8 +1,15 @@
 use clap::{App, Arg};
+use regex::{Error, Regex};
 use termcolor::{Color, ParseColorError};
 
-fn color_validator(color: String) -> Result<(), String> {
-  let _: Color = color
+fn regexp_validator(value: String) -> Result<(), String> {
+  let _: Regex = value.parse().map_err(|e: Error| e.to_string()).unwrap();
+
+  return Ok(());
+}
+
+fn color_validator(value: String) -> Result<(), String> {
+  let _: Color = value
     .parse()
     .map_err(|e: ParseColorError| e.to_string())
     .unwrap();
@@ -13,24 +20,48 @@ fn color_validator(color: String) -> Result<(), String> {
 pub fn build() -> App<'static, 'static> {
   return clap::app_from_crate!()
     .arg(
-      Arg::with_name("PATTERN")
+      Arg::with_name("pattern")
+        .value_name("PATTERN")
         .help("A regular expression used for highlighting.")
-        .required(true)
+        .takes_value(true)
+        .empty_values(false)
+        .required(false)
+        // .required_unless("regexp")
+        // .conflicts_with("regexp")
         .index(1),
     )
     .arg(
-      Arg::with_name("PATH")
+      Arg::with_name("path")
+        .value_name("PATH")
         .help("A file to highlight.")
+        .takes_value(true)
         .required(false)
         .index(2),
     )
     .arg(
-      Arg::with_name("COLOR")
+      Arg::with_name("regexp")
+        .value_name("REGEXP")
+        .short("e")
+        .long("regexp")
+        .alias("pattern")
+        .takes_value(true)
+        .empty_values(false)
+        // .conflicts_with("pattern")
+        .multiple(true)
+        .require_delimiter(true)
+        .validator(|s| regexp_validator(s))
+        .help("A pattern to search for."),
+    )
+    .arg(
+      Arg::with_name("color")
+        .value_name("COLOR")
         .short("c")
         .long("color")
-        .default_value("green")
+        .takes_value(true)
+        .empty_values(false)
+        .multiple(true)
+        .require_delimiter(true)
         .validator(|s| color_validator(s))
-        .help("A color for using highlight.")
-        .required(false),
+        .help("A color for using highlight."),
     );
 }
